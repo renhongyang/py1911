b506d235568e9d448b5a6fc04e44bf6943ad2aed
rhy <2605756330@qq.com>
2020-02-11 16:16:05 +0800

    创建数据库

--- end/demo1/bookdemo/booktest/models.py
+++ end/demo1/bookdemo/booktest/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title=models.CharField(max_length=20)
+    price=models.FloatField(default=0)
+    #upd_date=models.DateField(auto_now_add=True)
+    upd_date = models.DateField(default="2020-2-11")
+
+class Hero(models.Model):
+    name=models.CharField(max_length=20)
+    gender=models.CharField(max_length=6,choices=(('male','男'),('female','女')))
+    content=models.CharField(max_length=100)
+    book=models.ForeignKey(Book,on_delete=models.CASCADE)
