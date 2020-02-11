739122840d5b875f8a3dd834106e3a56def565a5
rhy <2605756330@qq.com>
2020-02-11 17:56:40 +0800

    填充表格内容

--- end/demo1/bookdemo/booktest/models.py
+++ end/demo1/bookdemo/booktest/models.py
@@ -7,8 +7,14 @@ class Book(models.Model):
     #upd_date=models.DateField(auto_now_add=True)
     upd_date = models.DateField(default="2020-2-11")
 
+    def __str__(self):
+        return self.title
+
 class Hero(models.Model):
     name=models.CharField(max_length=20)
-    gender=models.CharField(max_length=6,choices=(('male','男'),('female','女')))
+    gender=models.CharField(max_length=6,choices=(('male','男'),('female','女')),default='male')
     content=models.CharField(max_length=100)
     book=models.ForeignKey(Book,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
