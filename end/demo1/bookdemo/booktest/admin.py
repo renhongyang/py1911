2b6318df4a0036d933e88bc49b8579aa0ed4b298
rhy <2605756330@qq.com>
2020-02-13 20:00:04 +0800

    mvt补充

--- end/demo1/bookdemo/booktest/admin.py
+++ end/demo1/bookdemo/booktest/admin.py
@@ -5,32 +5,32 @@ from django.contrib.admin import ModelAdmin
 # Register your models here.
 
 
-# class HeroInline(admin.StackedInline):
-#     model = Hero
-#     # 关联个数
-#     extra = 1
-#
-# class BookAdmin(admin.ModelAdmin):
-#     # list_display：显示字段，可以点击列头进行排序
-#     list_display = ["title", "price", "pub_date"]
-#     # list_filter：过滤字段，过滤框会出现在右侧
-#     list_filter = ["title"]
-#     # search_fields：搜索字段，搜索框会出现在上侧
-#     search_fields = ["title"]
-#     # list_per_page：分页，分页框会出现在下侧
-#     list_per_page = 2
-#     # 再添加书的时候可以额外添加英雄
-#     inlines = [HeroInline]
-#
-# class HeroAdmin(admin.ModelAdmin):
-#     list_display = ["name", "gender", "content", "book"]
-#     # list_filter：过滤字段，过滤框会出现在右侧
-#     list_filter = ["name"]
-#     # search_fields：搜索字段，搜索框会出现在上侧
-#     search_fields = ["name", "book"]
-#     # list_per_page：分页，分页框会出现在下侧
-#     list_per_page = 2
+class HeroInline(admin.StackedInline):
+    model = Hero
+    #关联个数，可更改
+    extra = 1
 
-admin.site.register(Book)
+class BookAdmin(ModelAdmin):
+    # list_display显示字段
+    list_display = ("title", "price", "upd_date")
+    # list_filter：过滤字段，过滤框会出现在右侧
+    list_filter = ["title"]
+    # search_fields：搜索字段，搜索框会出现在上侧
+    search_fields = ["title"]
+    # list_per_page：分页，分页框会出现在下侧
+    list_per_page = 5
+    # 再添加书的时候可以额外添加英雄
+    inlines = [HeroInline]
+#
+class HeroAdmin(ModelAdmin):
+    list_display = ("name", "gender", "content", "book")
+    # list_filter：过滤字段，过滤框会出现在右侧
+    list_filter = ["name"]
+    # search_fields：搜索字段，搜索框会出现在上侧
+    search_fields = ("name", "book")
+    # list_per_page：分页，分页框会出现在下侧
+    list_per_page = 5
 
-admin.site.register(Hero)
+admin.site.register(Book,BookAdmin)
+
+admin.site.register(Hero,HeroAdmin)
