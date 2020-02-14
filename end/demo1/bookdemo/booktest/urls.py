f65234879cc6546de634e2515f8f53fb0b865696
rhy <2605756330@qq.com>
2020-02-14 19:58:16 +0800

    Django项目工程补充

--- end/demo1/bookdemo/booktest/urls.py
+++ end/demo1/bookdemo/booktest/urls.py
@@ -8,5 +8,10 @@ urlpatterns = [
     url(r'^about/$', views.about,name='about'),
     url(r'^detail/(\d+)/$', views.detail,name='detail'),
     url(r'^deletebook/(\d+)/$', views.deletebook,name='deletebook'),
+    url(r'^editbook/(\d+)/$', views.editbook,name='editbook'),
+    url(r'^addbook/$', views.addbook,name='addbook'),
+    url(r'^deletehero/(\d+)/$', views.deletehero,name='deletehero'),
+    url(r'^edithero/(\d+)/$', views.edithero,name='edithero'),
+    url(r'^addhero/(\d+)/$', views.addhero,name='addhero'),
 
 ]
--- end/demo1/bookdemo/booktest/views.py
+++ end/demo1/bookdemo/booktest/views.py
@@ -25,6 +25,67 @@ def deletebook(request,bookid):
     book.delete()
     return redirect(to='/')
 
+def addbook(request):
+    if request.method=="GET":
+        return render(request,'addbook.html')
+    elif request.method=="POST":
+        book=Book()
+        book.name=request.POST.get("booktitle")
+        book.gender = request.POST.get("bookprice")
+        book.upd_date=request.POST.get("bookupd_date")
+        book.save()
+        return redirect(to='/')
+
+def editbook(request,bookid):
+    book=Book.objects.get(id=bookid)
+    book.upd_date=str(book.upd_date)
+    if request.method=="GET":
+        return render(request,'editbook.html',{"book":book})
+    elif request.method=="POST":
+        book.title = request.POST.get("booktitle")
+        book.price = request.POST.get("bookprice")
+        book.upd_date = request.POST.get("bookupd_date")
+        book.save()
+        url = reverse("booktest:index")
+        return redirect(to=url)
+
+def deletehero(request,heroid):
+    hero=Hero.objects.get(id=heroid)
+    bookid=hero.book.id
+    hero.delete()
+    url=reverse("booktest:detail",args=(bookid,))
+    return redirect(to=url)
+
+def addhero(request,bookid):
+    if request.method=="GET":
+        return render(request,'addhero.html')
+    elif request.method=="POST":
+        hero=Hero()
+        hero.name=request.POST.get("heroname")
+        hero.gender = request.POST.get("herosex")
+        hero.content=request.POST.get("herocontent")
+        hero.book=Book.objects.get(id=bookid)
+        hero.save()
+        url = reverse("booktest:detail", args=(bookid,))
+        return redirect(to=url)
+
+def edithero(request,heroid):
+    hero=Hero.objects.get(id=heroid)
+    if request.method=="GET":
+        return render(request,'edithero.html',{"hero":hero})
+    elif request.method=="POST":
+        hero.name = request.POST.get("heroname")
+        hero.gender = request.POST.get("herosex")
+        hero.content = request.POST.get("herocontent")
+        hero.save()
+        url = reverse("booktest:detail", args=(hero.book.id,))
+        return redirect(to=url)
+
+
+
+
+
+
 
 def list(requset):
     return HttpResponse("这里是列表页")
