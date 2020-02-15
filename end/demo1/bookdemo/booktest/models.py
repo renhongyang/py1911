91b1045293102bb730b16fad648340a9cbc0992f
rhy <2605756330@qq.com>
2020-02-15 11:56:26 +0800

    字段选项 元选项 自定义管理类

--- end/demo1/bookdemo/booktest/models.py
+++ end/demo1/bookdemo/booktest/models.py
@@ -6,6 +6,8 @@ class Book(models.Model):
     price=models.FloatField(default=0)
     #upd_date=models.DateField(auto_now_add=True)
     upd_date = models.DateField(default="2020-2-11")
+    author=models.CharField(max_length=20,default="rhy")
+    desc=models.CharField(max_length=50,null=True,blank=True,db_column="description")
 
     def __str__(self):
         return self.title
@@ -14,7 +16,39 @@ class Hero(models.Model):
     name=models.CharField(max_length=20)
     gender=models.CharField(max_length=6,choices=(('male','男'),('female','女')),default='male')
     content=models.CharField(max_length=100)
-    book=models.ForeignKey(Book, on_delete=models.CASCADE)
+    book=models.ForeignKey(Book, on_delete=models.CASCADE,related_name= 'heros')
 
     def __str__(self):
         return self.name
+
+class UserManager(models.Manager):
+
+   """
+   自定义模型管理类，该模型不在具有objects对象
+   """
+   def deleteByTelephone(self,tele):
+       #django默认的objects是manager类型 *.objects.get
+       user=self.get(telephone=tele)
+       user.delete()
+
+   def createUser(self,tele):
+       #sele.model 可以获取模型类构造函数
+       user=self.model()
+       #user=user()
+       user.telephone=tele
+       user.save()
+
+class User(models.Model):
+    telephone=models.CharField(max_length=11,null=True,blank=True,verbose_name="手机号码")
+    #自定义管理字段后不在有objects对象，自定义了一个新的objects
+    objects=UserManager()
+    def __str__(self):
+        return self.telephone
+    class Meta():
+        db_table="用户类"
+        ordering=["-telephone"]
+        verbose_name="用户类型类a"
+        verbose_name_plural="用户类型类s"
+
+
+
