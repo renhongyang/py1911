15ab4a4d0f213438ad4369812bf0d56481cb6909
rhy <2605756330@qq.com>
2020-02-17 11:51:10 +0800

    投票应用（已完成90%）

--- end/demo1/bookdemo/polls/views.py
+++ end/demo1/bookdemo/polls/views.py
@@ -12,15 +12,23 @@ def poll_index(request):
 def poll_detail(request,pollid):
 
     poll=Poll.objects.get(id=pollid)
+    poll.num1 = int(poll.num1)
+    poll.num2 = int(poll.num2)
+    poll.num3 = int(poll.num3)
+    poll.num4 = int(poll.num4)
     if request.method=="GET":
         return render(request, 'poll_detail.html', {'poll': poll})
     elif request.method=="POST":
-        poll.num1  = request.POST.get("choose")
-        poll.num2 = request.POST.get("choose")
-        poll.num3 = request.POST.get("choose")
-        poll.num4 = request.POST.get("choose")
+        # poll.num1  = request.POST.get("choose")
+        # poll.num2 = request.POST.get("choose")
+        # poll.num3 = request.POST.get("choose")
+        # poll.num4 = request.POST.get("choose")
+        poll.num1 += 1
+        poll.num2 += 1
+        poll.num3 += 1
+        poll.num4 += 1
         poll.save()
-        url = reverse("polls:poll_detail",args=(pollid,))
+        url = reverse("polls:poll_show",args=(pollid,))
         return redirect(to=url)
 
 
