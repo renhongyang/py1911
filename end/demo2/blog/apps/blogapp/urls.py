9c684e01e87bbdc4900443cb0834b50b7a59fbbf
rhy <2605756330@qq.com>
2020-02-20 19:15:07 +0800

    blog项文章页面显示

--- end/demo2/blog/apps/blogapp/urls.py
+++ end/demo2/blog/apps/blogapp/urls.py
@@ -1,14 +1,17 @@
 # 引入路由绑定函数
 from django.conf.urls import url
 from . import views
-
+from django.views.generic.base import RedirectView
 
 app_name = "blogapp"
 
 urlpatterns = [
 
-    url(r'^$',views.index,name='index'),
+    url(r'^$', views.index,name='index'),
     #url(r'^single/(\d+)/$',views.single,name='single'),
-    url(r'^contact/$',views.contact,name='contact'),
+    url(r'^contact/$', views.contact, name='contact'),
     url(r'^single/$', views.single, name='single'),
+    # url(r'^favicon/$', views.favicon),
+    url(r'^favicon.ico$',RedirectView.as_view(url=r'static/img/favicon.ico')),
+
 ]
--- end/demo2/blog/apps/blogapp/views.py
+++ end/demo2/blog/apps/blogapp/views.py
@@ -1,12 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.template import loader
 from .models import *
 from django.http import HttpResponse,HttpResponseRedirect
+# Django自带了分页与分页器
+from django.core.paginator import Paginator,Page
 # Create your views here.
 
 def index(request):
     #return HttpResponse("首页")
-    return render(request,'index.html')
+    ads=Ads.objects.all()
+    #print(ads)
+    articles=Article.objects.all()
+    paginator = Paginator(articles, 2)
+    # 获取get请求中的页码参数  默认为1
+    num = request.GET.get("pagenum", 1)
+    page = paginator.get_page(num)
+    return render(request, 'index.html', {"ads": ads, "page": page})
+
 
 def single(request):
     #return HttpResponse("文章详情")
@@ -15,3 +25,6 @@ def single(request):
 def contact(request):
     #return HttpResponse("文章评论")
     return render(request,'contact.html')
+
+# def favicon(request):
+# #     return redirect(to="/static/favicon.ico")
